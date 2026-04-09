6f5c8a67a4c11b01de705dc4f439a5b2196b41b7
palak764 <pb459050@gmail.com>
2026-04-09 11:28:06 +0530

    new update

--- accounts/views.py
+++ accounts/views.py
@@ -533,7 +533,7 @@ def signup_customer(request):
         except Exception as e:
             return JsonResponse({"success": False, "error": str(e)})
 
-    return JsonResponse({"success": False, "error": "Invalid request method"})
+    return render(request, "accounts/signup_customer.html")
 
 
 def signup_employee(request):
