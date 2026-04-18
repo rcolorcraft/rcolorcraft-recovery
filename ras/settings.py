16439fa35bd16d07847534ae49c397030ae514db
palak764 <pb459050@gmail.com>
2026-04-18 16:07:24 +0530

    new updation

--- ras/settings.py
+++ ras/settings.py
@@ -326,3 +326,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
