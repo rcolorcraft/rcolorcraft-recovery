a297ba3a5c82a88dd91701d42d7dee925a221791
palak764 <pb459050@gmail.com>
2026-04-20 17:40:27 +0530

    fix update

--- employee/admin.py
+++ employee/admin.py
@@ -1,17 +1,8 @@
 from django.contrib import admin
-from django.utils.html import format_html
-from .models import ServiceImage  # ✅ same app → .models
+from .models import ServiceImage
 
 
 @admin.register(ServiceImage)
 class ServiceImageAdmin(admin.ModelAdmin):
-    list_display = ("id", "image_name", "status_badge", "created_at")
-    list_filter = ("is_approved",)
-
-    def status_badge(self, obj):
-        if obj.is_approved:
-            return format_html('<span style="color:green;">● Live</span>')
-        elif obj.is_verified_pic:
-            return format_html('<span style="color:orange;">● Approved</span>')
-        else:
-            return format_html('<span style="color:red;">● Pending</span>')
+    list_display = ["id", "image_name", "is_approved"]
+    list_editable = ["is_approved"]
--- employee/models.py
+++ employee/models.py
@@ -1,19 +1,31 @@
 from django.db import models
-from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 
 class ServiceImage(models.Model):
+    id = models.AutoField(primary_key=True)
     image_name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    from cloudinary.models import CloudinaryField
+
     image = CloudinaryField("image")
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     type_of_art = models.CharField(max_length=100)
-    userupload_id = models.IntegerField(default=0)
-
+    file_url = models.URLField(blank=True, null=True)
+    userupload_id = models.IntegerField(
+        default=0
+    )  # if you just want to store an ID manually
+    userupload_name = models.CharField(
+        max_length=255, default="Anonymous", null=False, blank=False
+    )  # uploader’s name as text
     is_verified_pic = models.BooleanField(default=False)
-    is_approved = models.BooleanField(default=False)
+    min_size = models.CharField(max_length=50, default="")
+    is_approved = models.BooleanField(
+        default=False
+    )  # 👈 YE ADD KARO # <-- New field for '10 * 60'
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = "service_images"
 
     def __str__(self):
-        return self.image_name
+        return f"{self.image_name} ({self.type_of_art})"
